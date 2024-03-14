e9b4f115fcf36e476d2eed69f6cd59d4fcde338e
Dmitriy <selezmsu@yandex.ru>
2024-03-14 23:21:57 +0300

    feat: UML Class Diagram.

--- src/Services/Catalog/MarketGoods.Catalog.Entities/Payment.cs
+++ src/Services/Catalog/MarketGoods.Catalog.Entities/Payment.cs
@@ -8,6 +8,7 @@ namespace MarketGoods.Catalog.Entities
         public Guid UserId { get; set; }
         public User User { get; set; }
         public Guid OrderId { get; set; }
+        public Order Order { get; set; }
         public PaymentStatus Status { get; set; }
     }
 }
